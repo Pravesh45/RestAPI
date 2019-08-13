575044f4a6ab419d06e50e1c8025e8b13dcb9234
Pravesh45 <sid.k.45.27@gmail.com>
2019-08-13 19:33:32 +0530

    Added POSTGRE environment variable and psycopg2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate,identity
@@ -8,7 +9,7 @@ from Resource.store import Store,StoreList
 
 app=Flask(__name__)
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS']=False
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///example.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///example.db')
 app.secret_key='lol'
 api=Api(app)
 
